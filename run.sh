83d06903c153dca4ebc5dbf1e143b5924a837d04
Sercan Ozturk <n.sercan.ozturk@gmail.com>
2025-06-17 01:15:54 +0300

    Update run.sh to include samplesheet path and remove resume option for improved workflow execution

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+export NXF_VER=24.10.5
+export SLACK_WEBHOOK="https://hooks.slack.com/services/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"
 clear && nextflow \
     -log logs/.nextflow.log \
     run main.nf \
     --skip_deeptmhmm true \
-    -resume
+    --samplesheet_path 'samplesheet.csv' \
